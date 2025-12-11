ec16c74a6d0139a5058bdfecea6e4ef1de9a2a6f
sai <sai@gmail.com>
2025-12-11 19:11:43 +0530

    java file

---
+++ file.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+    System.out.println("hkh s");
+    Scanner sc=new Scanner(System.in);
+    System.out.print("ente:");
+    int a=sc.nextInt();
+    System.out.print(a);
+
+
+
+}
+}
